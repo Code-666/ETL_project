6605beb030b19303833a111a12d2f4475ad1c088
JohnH <john.hillbom@gmail.com>
2023-04-29 10:32:19 +0200

    extract.py updated, linechart.py added

--- extract.py
+++ extract.py
@@ -1,7 +1,45 @@
 import requests
 import pandas as pd
+import json
 
-data = requests.get('https://dmigw.govcloud.dk/v1/forecastdata/collections/wam_dw/items?api-key=')
+#response = requests.get('https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json')
 
-print(data.json())
+def extract(response):
 
+    data = response.json()
+
+    with open("data.json", "w") as outfile:
+        json.dump(data, outfile)
+
+with open('data.json', 'r') as f:
+    data = json.load(f)
+
+def transform(data_index):
+    big_df = []
+    test = {}
+
+    test['Date'] = data['timeSeries'][data_index]['validTime']
+
+    for i in data['timeSeries'][data_index]['parameters']:
+        if i['name'] == 't':
+            test['Temperature'] = i['values'][0]
+
+    for i in data['timeSeries'][data_index]['parameters']:
+        if i['name'] == 'msl':
+            test['Air pressure'] = i['values'][0]
+
+    for i in data['timeSeries'][data_index]['parameters']:
+        if i['name'] == 'pcat':
+            test['Precipitation'] = i['values'][0]
+
+    big_df.append(test)
+    return pd.DataFrame(big_df)
+
+big_df = []
+for i in range(len(data['timeSeries'])):
+    print(i)
+    df = transform(i)
+    big_df.append(df)
+
+new_df = pd.concat(big_df, ignore_index=True)
+new_df.to_csv('test.csv')
---
+++ linechart.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the CSV file into a pandas DataFrame
+df = pd.read_csv('test.csv')
+
+print(df)
+
